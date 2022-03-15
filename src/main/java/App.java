34263a868b92bfdba69c3fab8a840d37f3c5ed10
Maximiliano Lombardia <mlombardia@itba.edu.ar>
2022-03-15 13:22:59 -0300

    fix: suggestion fix

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,14 +9,14 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
         ParticleGenerator generator = new ParticleGenerator(args);
-        CIM(generator);
-        BF(generator);
-    }
-
-    private static void CIM(ParticleGenerator generator) {
         List<Particle> particles = new ArrayList<>();
         generator.generate(particles);
         generator.placeParticles(particles);
+        CIM(particles);
+        BF(particles);
+    }
+
+    private static void CIM(List<Particle> particles) {
         long startTime = System.nanoTime();
         CellIndexMethod.run(particles);
         long endTime = System.nanoTime();
@@ -29,14 +29,10 @@ public class App {
         });
     }
 
-    private static void BF(ParticleGenerator generator) {
-        List<Particle> particles = new ArrayList<>();
-        generator.generate(particles);
-        generator.placeParticles(particles);
+    private static void BF(List<Particle> particles) {
         long startTime = System.nanoTime();
         BruteForceAlgorithm.run(particles);
         long endTime = System.nanoTime();
-
         System.out.printf("Brute Force %d ms\n", (endTime - startTime) / 1000000);
         particles.forEach(particle -> {
             if (particle.getNeighbours().size() != 0) {
